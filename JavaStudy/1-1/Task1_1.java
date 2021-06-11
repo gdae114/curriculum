74c5553925a32332a37294c79c23ed83c75c5935
gdae114 <85593800+gdae114@users.noreply.github.com>
2021-06-12 08:18:33 +0900

    課題提出
    java１章 Task 1_1

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,21 @@
+public class Task1_1 {
+
+  public static void main(String[] args) { 
+
+    int hogeInt = 100;
+
+    String hogeString = "合格";
+
+        //double型の変数hogeDoubleで宣言し、「0.2」で初期化。
+    double hogeDouble = 0.2 ;
+
+    System.out.println(hogeInt);
+
+    //変数hogeStringの値を表示
+    System.out.println(hogeString);
+
+
+    System.out.println(hogeDouble);
+
+  } 
+}
