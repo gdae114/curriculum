14d00fcff774c2b9021f68670b5e05fc315a020c
gdae114 <85593800+gdae114@users.noreply.github.com>
2021-06-13 08:10:55 +0900

    課題提出 Task1_1(再提出)
    Java　Task1_1の課題提出です。
    よろしくお願いします。

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -2,12 +2,16 @@ public class Task1_1 {
 
   public static void main(String[] args) { 
 
-    int hogeInt = 100;
+    int hogeInt = 50;
 
-    String hogeString = "合格";
+    String hogeString = "テスト";
 
         //double型の変数hogeDoubleで宣言し、「0.2」で初期化。
     double hogeDouble = 0.2 ;
+
+    int hogeInt = 100;
+
+    String hogeString = "合格";
 
     System.out.println(hogeInt);
 
